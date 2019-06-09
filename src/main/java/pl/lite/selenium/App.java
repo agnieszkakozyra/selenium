18ca18ed7afbd42cc154225459ab25a92ebf4894
Agnieszka <you@example.com>
2019-06-09 14:59:34 +0200

    junit - pierwsze szlify

--- src/main/java/pl/lite/selenium/App.java
+++ src/main/java/pl/lite/selenium/App.java
@@ -15,28 +15,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.setProperty("webdriver.chrome.driver", "C:\\programiki\\chromedriver_win32\\chromedriver.exe");
-    
-        driver=new ChromeDriver();
-        driver.get("http://newtours.demoaut.com");
-        driver.findElement(By.linkText("CONTACT")).click();
-        sleep(2);
-        driver.findElement(By.linkText("SUPPORT")).click();
-        sleep(2);
-        driver.findElement(By.linkText("REGISTER")).click();
-        sleep(2);
-        driver.findElement(By.linkText("SIGN-ON")).click();
-        sleep(2);
-        driver.quit();
         
     }
-    public static void sleep(int seconds) {
-    	try {
-			Thread.sleep(seconds*1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-    }
+   
 }
---
+++ src/test/java/pl/lite/selenium/Init.java
@@ -0,0 +1,27 @@
+package pl.lite.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Init {
+
+	static WebDriver driver;
+	public static WebDriver getDriver()
+{
+		 System.setProperty("webdriver.chrome.driver", "C:\\programiki\\chromedriver_win32\\chromedriver.exe");
+		    
+	        driver=new ChromeDriver();
+	        driver.get("http://newtours.demoaut.com");
+	        return driver;
+			
+		}
+	 public static void sleep(int seconds) {
+	    	try {
+				Thread.sleep(seconds*1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+}
