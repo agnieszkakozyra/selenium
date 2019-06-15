15cfe2ee22287fad27eb236a8c2b60d34684504b
Agnieszka <you@example.com>
2019-06-15 11:20:52 +0200

    junit fin

--- src/test/java/pl/lite/selenium/Init.java
+++ src/test/java/pl/lite/selenium/Init.java
@@ -6,22 +6,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Init {
 
 	static WebDriver driver;
-	public static WebDriver getDriver()
-{
-		 System.setProperty("webdriver.chrome.driver", "C:\\programiki\\chromedriver_win32\\chromedriver.exe");
-		    
-	        driver=new ChromeDriver();
-	        driver.get("http://newtours.demoaut.com");
-	        return driver;
-			
-		}
-	 public static void sleep(int seconds) {
-	    	try {
-				Thread.sleep(seconds*1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 
-	    }
+	public static WebDriver getDriver() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\programiki\\chromedriver_win32\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+		driver.get("http://newtours.demoaut.com");
+		return driver;
+
+	}
+
+	public static void sleep(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void close() {
+		driver.quit();
+	}
+	
+	
 }
