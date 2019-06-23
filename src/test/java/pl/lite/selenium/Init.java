58a0c250892494ec7aba7b9cd93399c25b079f13
Agnieszka <you@example.com>
2019-06-23 15:10:07 +0200

    ze zmiana z dockerem

--- src/test/java/pl/lite/selenium/Init.java
+++ src/test/java/pl/lite/selenium/Init.java
@@ -1,7 +1,12 @@
 package pl.lite.selenium;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Init {
 
@@ -11,10 +16,26 @@ public class Init {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\programiki\\chromedriver_win32\\chromedriver.exe");
 
-		driver = new ChromeDriver();
+		//driver = new ChromeDriver();
+		
+		
+		URL url= null;
+		
+		try {
+			url= new URL ("http://192.168.0.101:4444/wd/hub");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		DesiredCapabilities capability=DesiredCapabilities.chrome();
+		
+		driver= new RemoteWebDriver(url,capability);
+		
 		driver.get("http://newtours.demoaut.com");
+		
 		return driver;
-
+		
 	}
 
 	public static void sleep(int seconds) {
